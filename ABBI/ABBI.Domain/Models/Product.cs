98d533ea06342e472562be7453d8b389c5ca7cbd
Berihun Hadis <berihunhadis@gmail.com>
2021-08-25 17:33:45 +0300

    this is sprint one models

---
+++ ABBI/ABBI.Domain/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPICompleted.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string SupplierId { get; set; }
+        public string CategoryId { get; set; }
+        public float Quantity { get; set; }
+        public float UnitPrice { get; set; }
+    }
+}
