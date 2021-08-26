cc3ee55f675cc1d9f4882945cae88f7174a2fdd1
Belay Retta <tony.retta@gmail.com>
2021-08-26 11:08:57 +0300

    OrderItemModel and OrderModel added to domain model

---
+++ ABBI/ABBI.Domain/Models/OrderItemModel.cs
@@ -0,0 +1,23 @@
+﻿using Order.Domain.Seed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Domain.Models
+{
+    public class OrderItemModel: BaseAuditModel
+    {
+        public decimal Qty { get; protected set; }
+        public string Name { get; protected set; }
+        public decimal Price { get; protected set; }
+        public OrderItemModel(decimal qty, string name, decimal price)
+        {
+            Price = price;
+            Qty = qty;
+            Name = name;
+
+        }
+    }
+}
---
+++ ABBI/ABBI.Domain/Models/OrderModel.cs
@@ -0,0 +1,21 @@
+﻿using Order.Domain.Seed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Domain.Models
+{
+    public class OrderModel : BaseAuditModel
+    {
+        public decimal TotalPrice { get; set; }
+        public string UserName { get; set; }
+       
+        public List<OrderItemModel> OrderItems { get; set; }
+        public OrderModel()
+        {
+
+        }
+    }
+}
