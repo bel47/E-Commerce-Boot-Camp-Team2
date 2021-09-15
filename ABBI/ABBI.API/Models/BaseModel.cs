3cf230803979b42f1f20f3dcab05ae6d8f80e32e
Belay Retta <tony.retta@gmail.com>
2021-09-15 14:06:12 +0300

    Angular removed

--- ABBI/ABBI.API/Models/BaseModel.cs
+++ ABBI/ABBI.API/Models/BaseModel.cs
@@ -7,7 +7,7 @@ namespace ABBI.API.Models
 {
     public abstract class BaseModel
     {
-        public Guid? Id { get; set; }
+       // public Guid? Id { get; set; }
         public bool? IsActive { get; set; }
         public DateTime? CreatedDate { get; set; }
         public abstract T MapToEntity<T>() where T : class;
--- ABBI/ABBI.API/Startup.cs
+++ ABBI/ABBI.API/Startup.cs
@@ -43,80 +43,80 @@ namespace ABBI.API
             //Register Infrastructure
             services.AddInfrastructureServices(Configuration);
 
-            /*          services.AddSwaggerGen(c =>
+            services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ABBI.API", Version = "v1" });
-            });*/
+            });
 
             // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
+/*            services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
-            });
+            });*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            /* if (env.IsDevelopment())
-                {
-                    app.UseDeveloperExceptionPage();
-                    app.UseSwagger();
-                    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ABBI.API v1"));
-                }
-
-                //   app.UseHttpsRedirection();
-
-                app.UseRouting();
-
-                app.UseStaticFiles();
-                // app.UseAuthentication();
-                //app.UseAuthorization();
-
-                app.UseEndpoints(endpoints =>
-                {
-                    endpoints.MapControllers();
-                });*/
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ABBI.API v1"));
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            if (!env.IsDevelopment())
-            {
-                app.UseSpaStaticFiles();
-            }
+            //   app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+            // app.UseAuthentication();
+            //app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
 
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
+            /* if (env.IsDevelopment())
+             {
+                 app.UseDeveloperExceptionPage();
+             }
+             else
+             {
+                 app.UseExceptionHandler("/Error");
+                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                 app.UseHsts();
+             }
 
-                spa.Options.SourcePath = "ClientApp";
+             app.UseHttpsRedirection();
+             app.UseStaticFiles();
+             if (!env.IsDevelopment())
+             {
+                 app.UseSpaStaticFiles();
+             }
 
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
-            });
+             app.UseRouting();
+
+             app.UseEndpoints(endpoints =>
+             {
+                 endpoints.MapControllerRoute(
+                     name: "default",
+                     pattern: "{controller}/{action=Index}/{id?}");
+             });
+
+             app.UseSpa(spa =>
+             {
+                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                 // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                 spa.Options.SourcePath = "ClientApp";
+
+                 if (env.IsDevelopment())
+                 {
+                     spa.UseAngularCliServer(npmScript: "start");
+                 }
+             });*/
         }
     }
 }
