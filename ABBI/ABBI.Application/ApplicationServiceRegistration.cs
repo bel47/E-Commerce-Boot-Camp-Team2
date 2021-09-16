f622dd79e3152d2ce0d2519064fdab6c2b8c786f
Belay Retta <tony.retta@gmail.com>
2021-09-16 15:58:24 +0300

    application modfied

---
+++ ABBI/ABBI.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ABBI.Application.Features;
+using ABBI.Domain.Interfaces.Facade;
+
+namespace ABBI.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services) 
+        {
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IBasketService, BasketService>();
+
+            return services;
+        }
+    }
+}
